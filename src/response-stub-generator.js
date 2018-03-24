71570c2558b4778af1971994e0e17a6a076e3af6
trmollet <tristan.mollet@soprabanking.com>
2018-03-24 17:18:55 +0100

    update

--- src/response-stub-generator.js
+++ src/response-stub-generator.js
@@ -7,21 +7,29 @@ module.exports = function (context) {
     return;
   }
   if(stub_generator.active === true){
-    //generate_response_stub(context.response);
+    generate_response_stub(context.response, stub_generator.file_path);
   } 
 }
 
-function generate_response_stub(response) {
+function generate_response_stub(response,file_path) {
 
   const response_status = response.getStatusCode();
 
   var jsonfile = require('jsonfile');
 
-  var file = '/Users/trmollet/perso/dev/stub-response.json';
+  var stub_request = file_path + '/stub-request.json';
 
-  var stub = {
+  var file = file_path + '/stub.json';
+
+
+
+  var stub_response = {
     status: response_status
   };
+
+  jsonfile.readFile(stub_request, function(err, obj){
+   console.log(obj);
+  });
 
   console.log(stub);
 
